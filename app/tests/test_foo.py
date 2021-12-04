3db1111a589a3f3b1de0646db598256f7d4bebad
Fang Liu <fangyiliu@gmail.com>
2021-12-03 21:25:12 -0800

    Label test Given When Then

--- app/tests/test_foo.py
+++ app/tests/test_foo.py
@@ -7,8 +7,14 @@ def test_hello_world():
     assert "foo" != "bar"
 
 def test_ping(client):
+    # Given
+    # client
+
+    # When
     url = reverse("ping")
     response = client.get(url)
     content = json.loads(response.content)
+
+    # Then
     assert response.status_code == 200
     assert content["ping"] == "pong!"
