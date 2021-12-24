10f100f9cf730b8b00e7108105b5cb2f25a65439
Fang Liu <fangyiliu@gmail.com>
2021-12-24 15:41:19 -0800

    Test for update movie
    Tests should fail
    docker-compose exec movies pytest -p no:warnings

--- app/tests/movies/test_views.py
+++ app/tests/movies/test_views.py
@@ -98,3 +98,46 @@ def test_remove_movie(client, add_movie):
 def test_remove_movie_incorrect_id(client):
     resp = client.delete(f"/api/movies/99/")
     assert resp.status_code == 404
+
+@pytest.mark.django_db
+def test_update_movie(client, add_movie):
+    movie = add_movie(title="The Big Lebowski", genre="comedy", year="1998")
+
+    resp = client.put(
+        f"/api/movies/{movie.id}/",
+        {"title": "The Big Lebowski", "genre": "comedy", "year": "1997"},
+        content_type="application/json"
+    )
+    assert resp.status_code == 200
+    assert resp.data["title"] == "The Big Lebowski"
+    assert resp.data["year"] == "1997"
+
+    resp_two = client.get(f"/api/movies/{movie.id}/")
+    assert resp_two.status_code == 200
+    assert resp_two.data["title"] == "The Big Lebowski"
+    assert resp.data["year"] == "1997"
+
+
+@pytest.mark.django_db
+def test_update_movie_incorrect_id(client):
+    resp = client.put(f"/api/movies/99/")
+    assert resp.status_code == 404
+
+
+@pytest.mark.django_db
+def test_update_movie_invalid_json(client, add_movie):
+    movie = add_movie(title="The Big Lebowski", genre="comedy", year="1998")
+    resp = client.put(f"/api/movies/{movie.id}/", {}, content_type="application/json")
+    assert resp.status_code == 400
+
+
+@pytest.mark.django_db
+def test_update_movie_invalid_json_keys(client, add_movie):
+    movie = add_movie(title="The Big Lebowski", genre="comedy", year="1998")
+
+    resp = client.put(
+        f"/api/movies/{movie.id}/",
+        {"title": "The Big Lebowski", "genre": "comedy"},
+        content_type="application/json",
+    )
+    assert resp.status_code == 400
