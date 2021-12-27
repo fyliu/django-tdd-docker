522533c3e9c149e50a8b9326592831efcc407c01
Fang Liu <fangyiliu@gmail.com>
2021-12-27 13:55:53 -0800

    Unit test add movie

--- app/tests/movies/test_views_unit.py
+++ app/tests/movies/test_views_unit.py
@@ -5,15 +5,31 @@ from movies.views import MovieSerializer, MovieDetail, Movie
 
 
 def test_add_movie(client, monkeypatch):
-    pass
+    payload = {"title": "The Big Lebowski", "genre": "comedy", "year": "1998"}
+
+    def mock_create(self, payload):
+        return "The Big Lebowski"
+
+    monkeypatch.setattr(MovieSerializer, "create", mock_create)
+    monkeypatch.setattr(MovieSerializer, "data", payload)
+
+    resp = client.post("/api/movies/", payload, content_type="application/json")
+    assert resp.status_code == 201
+    assert resp.data["title"] == "The Big Lebowski"
 
 
 def test_add_movie_invalid_json(client):
-    pass
+    resp = client.post("/api/movies/", {}, content_type="application/json")
+    assert resp.status_code == 400
 
 
 def test_add_movie_invalid_json_keys(client):
-    pass
+    resp = client.post(
+        "/api/movies/",
+        {"title": "The Big Lebowski", "genre": "comedy"},
+        content_type="application/json",
+    )
+    assert resp.status_code == 400
 
 
 def test_get_single_movie(client, monkeypatch):
